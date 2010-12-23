69348b18d96ebc95e700bae569f1cecd4171cafc
Chris Cantu <christopher.cantu@gmail.com>
2010-12-23 11:29:11 -0600

    Updated Moviecontroller to support create, update & delete scenarios

--- movie-rest/grails-app/controllers/com/rackspace/MovieController.groovy
+++ movie-rest/grails-app/controllers/com/rackspace/MovieController.groovy
@@ -15,35 +15,48 @@ class MovieController {
 	}
 	
 	def update = {
-		if (params.id && Movie.exists(params.id)) {
-			def m = Movie.get(params.id)
-			m.properties = params
-			m.save()
-			render "$m updated successfully"
+		def m = Movie.get(params.id)
+		
+		if (m) {
+			m.properties = request.JSON
+			
+			def message
+			if (m.validate() && m.save()) {
+				response.status = HttpServletResponse.SC_OK
+				message = "$m updated successfully"
+			} else {
+				response.status = HttpServletResponse.SC_BAD_REQUEST
+				message = "$m - Error:  ${m.errors}"
+			}
+			
+			render message
 		} else {
+			response.status = HttpServletResponse.SC_NOT_FOUND
 			render "No such movie"
 		}
 	}
 	
 	def save = {
-		def m = new Movie(title: params.title, genre: Genre.get(params.genre), 
-			media: Media.get(params.media), year: new Date(year: params.year as Integer), rating: params.rating)
-			
+		def m = new Movie(params)
+		m.mediaId = params.mediaId
+		m.genreId = params.genreId
+
 		if (m.validate() && m.save()) {
 			response.status = HttpServletResponse.SC_CREATED
 			render("Movie successfully created")
 		} else {
 			response.status = HttpServletResponse.SC_BAD_REQUEST
-			render "Error creating movie"
+			render "Error creating movie - Error(s): ${m.errors}"
 		}
 	}
 	
 	def delete = {
 		def m = Movie.get(params.id)
-		
-		if (m.delete()) {
+		if (m) {
+			m.delete()
 			render "Movie deleted successfully"
 		} else {
+			response.status = HttpServletResponse.SC_NOT_FOUND
 			render "Unable to delete movie"
 		}
 	}
