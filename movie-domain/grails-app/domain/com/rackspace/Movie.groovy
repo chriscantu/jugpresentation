c65827676161d338eef33a38965da4b744b39f61
Chris Cantu <christopher.cantu@gmail.com>
2010-12-23 11:27:51 -0600

    Updated Movie domain to handle string parameters

--- movie-domain/grails-app/domain/com/rackspace/Movie.groovy
+++ movie-domain/grails-app/domain/com/rackspace/Movie.groovy
@@ -4,13 +4,33 @@ class Movie {
 	
 	String title
 	Integer rating
-	Date year
+	Integer year
 	Genre genre
 	Media media
 
     static constraints = {
-		title(blank:false, size: 3..100)
+		title(unique: true, blank:false, size: 3..100)
 		rating(min:1, max:5)
 		year(nullable:false)
-    }	
+    }
+
+	static transients = ['genreId', "mediaId"]
+
+	void setGenreId(genreId) {
+		genre = Genre.get(genreId)
+	}
+	
+	Integer getGenreId() {
+		genre?.id
+	}
+	
+	void setMediaId(mediaId) {
+		media = Media.get(mediaId)
+	}
+	
+	Integer getMediaId(){
+		media?.id
+	}
+	
+	String toString() { "Movie - '$title'" }
 } 
