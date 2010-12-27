bb5c600fb46b422d86bdf5395ed8fb07db5b64f0
Chris Cantu <christopher.cantu@gmail.com>
2010-12-27 11:01:04 -0600

    Added show method on the MovieService

--- moviestore/grails-app/services/com/rackspace/MovieService.groovy
+++ moviestore/grails-app/services/com/rackspace/MovieService.groovy
@@ -5,13 +5,22 @@ import grails.converters.*
 class MovieService {
 
     static transactional = false
-	static restEndpoint = "http://localhost:8082/movie-rest/"
+	static restEndpoint = "http://localhost:8081/movie-rest/"
 
 	def listMovies() {
 		def response
 		withRest(uri: restEndpoint){
 			response = get(path:"movie").data.toString()
 		}
+		JSON.parse(response)
+	}
+	
+	def show(id){
+		def response 
+		withRest(uri: restEndpoint){
+			response = get(path:"movie/$id")?.data.toString()
+		}
+		
 		JSON.parse(response)
 	}
 }
