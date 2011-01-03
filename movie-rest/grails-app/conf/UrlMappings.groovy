35fe9997e02add421e7cc4eae5597c5e2eb1f78d
Chris Cantu <christopher.cantu@gmail.com>
2011-01-02 23:48:05 -0600

    Removed commented out code

--- movie-rest/grails-app/conf/UrlMappings.groovy
+++ movie-rest/grails-app/conf/UrlMappings.groovy
@@ -1,14 +1,7 @@
 class UrlMappings {
 
 	static mappings = {
-/*		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
 
-		"/"(view:"/index")
-		"500"(view:'/error')*/
 		"/$controller/$id?" {
 			action = [GET:"show", PUT: "update", DELETE:"delete", POST:"save"]
 		}
