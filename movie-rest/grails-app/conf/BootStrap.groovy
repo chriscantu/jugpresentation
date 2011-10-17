d9ac1321f562bf92db50f1850269891ecf49407d
Chris <christopher.cantu@gmail.com>
2011-10-16 20:50:57 -0500

    Added test bed of data to the bootstrap.

--- movie-rest/grails-app/conf/BootStrap.groovy
+++ movie-rest/grails-app/conf/BootStrap.groovy
@@ -3,11 +3,37 @@ import com.rackspace.*
 class BootStrap {
 
     def init = { servletContext ->
-		new Genre(type: "Action").save()
-		new Genre(type: "Comedy").save()
-		new Media(type: "DVD").save()
-		new Media(type: "Blu-Ray").save()
+        
+        def blu = new Media(type:"BluRay")
+        def dvd = new Media(type: "DVD")
+        
+        blu.save()
+        dvd.save()
+        
+        def act = new Genre(type: "Action")
+        def com = new Genre(type: "Comedy")
+        
+        act.save()
+        com.save()
+        
+        createMovie( ["Iron Man", 2008, 4, act, blu ] )
+        createMovie( ["Thor", 2011, 3, act, blu ] )
+        createMovie( ["3 Amigos", 1986, 5, com, dvd ] )
+        createMovie( ["300", 2006, 5, act, blu ] )
+        createMovie( ["Topgun", 1986, 4, act, dvd] )
+        createMovie( ["The Lion King", 1994, 4, com, dvd ] )
+        createMovie( ["Godfather II", 1972, 5, com, dvd ] )
+        createMovie( ["Pulp Fiction", 1994, 4, act, dvd ] )
+        createMovie( ["The Dark Knight", 2008, 5, act, dvd ] )
+        createMovie( ["Fight Club", 1999, 5, act, dvd ] )
+        createMovie( ["Aliens", 1986, 5, act, dvd ] )
     }
+    
+    def createMovie(list) {
+        new Movie( title: list.get(0), year: list.get(1), rating: list.get(2),
+            genre: list.get(3), media: list.get(4) ).save()
+    }
+
     def destroy = {
     }
 }
